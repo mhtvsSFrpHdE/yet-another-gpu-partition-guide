a41c4c4359ab60d68d60ce1fef40ed86bd7bc480
mhtvsSFrpHdE <2bemperor47@gmail.com>
2025-08-24 16:21:53 +0800

    Copy exist driver settings

--- Tools/CollectFiles.ps1
+++ Tools/CollectFiles.ps1
@@ -5,7 +5,7 @@ $cardName = "1080 Ti"
 $filesToCollectArray = @()
 
 $driverFileListBegin = $false
-$dxDiagFile = Get-Content -Path "DxDiag.txt"
+$dxDiagFile = Get-Content -Path "$PSScriptRoot\DxDiag.txt"
 foreach ($line in $dxDiagFile) {
     # Find card name line
     $line = $line.Trim()
@@ -40,6 +40,7 @@ $installScriptPath = "C:\GpuDriverExtra\install.ps1"
 $uninstallScriptPath = "C:\GpuDriverExtra\uninstall.ps1"
 # Overwrite exist
 New-Item -ItemType File -Path $installScriptPath -Force
+New-Item -ItemType File -Path $uninstallScriptPath -Force
 foreach ($file in $filesToCollectArray) {
     $src = $file[0]
     $dest = $file[1]
@@ -48,8 +49,16 @@ foreach ($file in $filesToCollectArray) {
     Copy-Item -Path $src -Destination $dest -Force
 
     # install command
+    "New-Item -ItemType File -Path `"$src`" -Force" | Out-File $installScriptPath -Append
     "Copy-Item -Path `"$dest`" -Destination `"$src`"" | Out-File $installScriptPath -Append
 
     # uninstall command
     "Remove-Item -Path `"$dest`"" | Out-File $uninstallScriptPath -Append
 }
+
+# Copy exist driver settings
+Remove-Item -Path "C:\GpuDriverExtra\ProgramData\NVIDIA Corporation" -Recurse
+Copy-Item -Path "C:\ProgramData\NVIDIA Corporation" -Destination "C:\GpuDriverExtra\ProgramData\NVIDIA Corporation" -Recurse
+# Append to install and uninstall script
+"Copy-Item -Path `"C:\GpuDriverExtra\ProgramData\NVIDIA Corporation`" -Destination `"C:\ProgramData`" -Recurse" | Out-File $installScriptPath -Append
+"Remove-Item -Path `"C:\ProgramData\NVIDIA Corporation`" -Recurse" | Out-File $uninstallScriptPath -Append
