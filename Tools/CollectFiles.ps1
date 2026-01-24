b8b3048e38838b02fe8870b3ad0e0ba70648b7a4
mhtvsSFrpHdE <2bemperor47@gmail.com>
2026-01-25 05:21:30 +0800

    Fix installed inf GPU driver files randomly gone

--- Tools/CollectFiles.ps1
+++ Tools/CollectFiles.ps1
@@ -1,12 +1,20 @@
 $cardName = "1080 Ti"
 
 # Prepare export folder
-Remove-Item -Path "C:\GpuDriverExtra" -Recurse
+Remove-Item -Path "C:\GpuDriver" -Recurse
 
-# Declare files array structure
+# Declare inplace files array structure
+#   these copy from src to dest, then copy back to src on install
 # [0]: file in Windows dir
-# [1]: file copy to GpuDriverExtra
-$filesToCollectArray = @()
+# [1]: file copy to GpuDriver
+$inplaceFilesToCollectArray = @()
+
+# Declare HostDriverStore files array structure
+#   these copy from src to dest, then copy back to HostDriverStore on install
+# [0]: file in DriverStore dir
+# [1]: file copy to GpuDriver
+# [2]: copy to where on install
+$hostDriverStoreFilesToCollectArray = @()
 
 # Parse DxDiag.txt to get driver files
 $driverFileListBegin = $false
@@ -30,59 +38,38 @@ foreach ($line in $dxDiagFile) {
         $removeDescription = $removeDriverPrefix.Split(",", 2)[0]
         $fileToCollect = $removeDescription
 
-        # Ignore file will be installed by inf
+        $fileCopyDest = ""
+        # File will be installed by inf, copy to HostDriverStore
         if ($fileToCollect.Contains("DriverStore")){
-            continue
+            $fileCopyDest = $fileToCollect -ireplace [regex]::Escape("C:\WINDOWS\System32\DriverStore"), "C:\GpuDriver\HostDriverStore"
+            $fileInstallTo = $fileToCollect -ireplace [regex]::Escape("C:\WINDOWS\System32\DriverStore"), "C:\WINDOWS\System32\HostDriverStore"
+            $hostDriverStoreFilesToCollectArray += , (@($fileToCollect, $fileCopyDest, $fileInstallTo))
         }
-
-        $fileCopyDest = $fileToCollect.Replace("C:\WINDOWS", "C:\GpuDriverExtra\Windows")
-        $filesToCollectArray += , (@($fileToCollect, $fileCopyDest))
+        else{
+            $fileCopyDest = $fileToCollect -ireplace [regex]::Escape("C:\WINDOWS"), "C:\GpuDriver\Windows"
+            $inplaceFilesToCollectArray += , (@($fileToCollect, $fileCopyDest))
+        }
     }
 }
 
 # Create install, upgrade and uninstall script
-$installScriptPath = "C:\GpuDriverExtra\install.ps1"
-$installWithDataScriptPath = "C:\GpuDriverExtra\install_with_data.ps1"
+$installScriptPath = "C:\GpuDriver\install.ps1"
+$installWithDataScriptPath = "C:\GpuDriver\install_with_data.ps1"
 # File name _z has less priority to be autocomplete on console
-$uninstallScriptPath = "C:\GpuDriverExtra\uninstall_z_remove_data.ps1"
-$uninstallKeepDataScriptPath = "C:\GpuDriverExtra\uninstall_keep_data.ps1"
+$uninstallScriptPath = "C:\GpuDriver\uninstall_z_remove_data.ps1"
+$uninstallKeepDataScriptPath = "C:\GpuDriver\uninstall_keep_data.ps1"
 # Overwrite exist
 New-Item -ItemType File -Path $installScriptPath -Force
 New-Item -ItemType File -Path $installWithDataScriptPath -Force
 New-Item -ItemType File -Path $uninstallScriptPath -Force
 New-Item -ItemType File -Path $uninstallKeepDataScriptPath -Force
 
-# Write upgrade (uninstall keep data) script header
-# Code from InstallGuestDriver, remove display driver from HostDriverStore
-$uninstallKeepDataScriptContent = @'
-$drivers = Get-WindowsDriver -Online -All
-$targetDriver = $drivers | Where-Object { $_.ClassName -EQ "Display" -and $_.ProviderName -Like "NVIDIA" }
-$targetDriver
-$targetDriverOemFile = $targetDriver.Driver
-$targetDriverOemFile
-
-$targetDriverIndex = 0
-$driverInstallDir = $targetDriver[$targetDriverIndex].OriginalFileName
-$driverInstallDir = Split-Path $driverInstallDir
-$driverInstallDirName = Split-Path -Leaf $driverInstallDir
-$hostDriverStoreDir = "C:\Windows\System32\HostDriverStore\FileRepository"
-$hostDriverStoreDir = Join-Path $hostDriverStoreDir $driverInstallDirName
-
-# Preview and verify path
-$driverInstallDir
-$hostDriverStoreDir
-
-pnputil /delete-driver "$targetDriverOemFile"
-Remove-Item -Path "$hostDriverStoreDir" -Recurse
-
-'@
-$uninstallKeepDataScriptContent | Out-File $uninstallKeepDataScriptPath -Append
-
 # Write each file to different script
-foreach ($file in $filesToCollectArray) {
+foreach ($file in $inplaceFilesToCollectArray) {
     $src = $file[0]
     $dest = $file[1]
-    # Create parent folder if not exist and copy file
+
+    # Create parent folder if not exist and copy file to GpuDriver export folder
     New-Item -ItemType File -Path $dest -Force
     Copy-Item -Path $src -Destination $dest -Force
 
@@ -98,12 +85,33 @@ foreach ($file in $filesToCollectArray) {
     # upgrade command
     "Remove-Item -Path `"$dest`"" | Out-File $uninstallKeepDataScriptPath -Append
 }
+foreach ($file in $hostDriverStoreFilesToCollectArray) {
+    $src = $file[0]
+    $dest = $file[1]
+    $installTo = $file[2]
+
+    # Create parent folder if not exist and copy file to GpuDriver export folder
+    New-Item -ItemType File -Path $dest -Force
+    Copy-Item -Path $src -Destination $dest -Force
+
+    # install command
+    "New-Item -ItemType File -Path `"$installTo`" -Force" | Out-File $installScriptPath -Append
+    "Copy-Item -Path `"$dest`" -Destination `"$installTo`"" | Out-File $installScriptPath -Append
+    "New-Item -ItemType File -Path `"$installTo`" -Force" | Out-File $installWithDataScriptPath -Append
+    "Copy-Item -Path `"$dest`" -Destination `"$installTo`"" | Out-File $installWithDataScriptPath -Append
+
+    # uninstall command
+    "Remove-Item -Path `"$installTo`"" | Out-File $uninstallScriptPath -Append
+
+    # upgrade command
+    "Remove-Item -Path `"$installTo`"" | Out-File $uninstallKeepDataScriptPath -Append
+}
 
 # Copy exist driver settings to export folder
-Copy-Item -Path "C:\ProgramData\NVIDIA Corporation" -Destination "C:\GpuDriverExtra\ProgramData\NVIDIA Corporation" -Recurse
+Copy-Item -Path "C:\ProgramData\NVIDIA Corporation" -Destination "C:\GpuDriver\ProgramData\NVIDIA Corporation" -Recurse
 
 # Append to install with data
-"Copy-Item -Path `"C:\GpuDriverExtra\ProgramData\NVIDIA Corporation`" -Destination `"C:\ProgramData`" -Recurse" | Out-File $installWithDataScriptPath -Append
+"Copy-Item -Path `"C:\GpuDriver\ProgramData\NVIDIA Corporation`" -Destination `"C:\ProgramData`" -Recurse" | Out-File $installWithDataScriptPath -Append
 
 # Append to uninstall script
 "Remove-Item -Path `"C:\ProgramData\NVIDIA Corporation`" -Recurse" | Out-File $uninstallScriptPath -Append
